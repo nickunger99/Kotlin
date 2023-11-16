5b33ac38afefda0f2acbff893e97f17882101d0c
nickunger99 <nicolas.unger@yahoo.com.br>
2023-11-16 11:16:44 -0300

    construtor

--- AluGames/src/main/kotlin/Main.kt
+++ AluGames/src/main/kotlin/Main.kt
@@ -20,8 +20,15 @@ fun main(args: Array<String>) {
     val json = response.body()
     println(json)
 
-    val meuJogo = Jogo()
-    meuJogo.titulo = "Batman: Arkham Asylum Game of the Year Edition"
-    meuJogo.capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1681938587"
-    print(meuJogo)
+    val meuJogo = Jogo(
+        "Batman: Arkham Asylum Game of the Year Edition",
+        "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1681938587"
+    )
+    println(meuJogo)
+
+    val novoJogo = Jogo(
+        capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1681938587",
+        titulo = "Batman: Arkham Asylum Game of the Year Edition"
+    )
+    println(novoJogo)
 }
